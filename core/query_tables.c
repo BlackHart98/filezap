49be5188e1f334c071dc0fe7a31923955bf280a4
Pius Arhanbhunde <pjacks419@gmail.com>
2025-11-03 23:15:28 +0100

    Update query_tables.c

--- core/query_tables.c
+++ core/query_tables.c
@@ -4,7 +4,11 @@ extern int fz_query_required_chunk_list(fz_ctx_t *ctx, fz_file_manifest_t *mnfst
     int result = 1;
     fz_chunk_t *buffer = NULL;
     size_t local_nchunk = 0;
+    sqlite3_stmt *insert = NULL;
+    sqlite3_stmt *stmt = NULL;
     int ret;
+
+    /* SQL templates */
     const char *create_tbl_sql = "CREATE TEMP TABLE IF NOT EXISTS temp_manifest_chunks (chunk_checksum INTEGER);";
     const char *insert_sql = "INSERT INTO temp_manifest_chunks (chunk_checksum) VALUES (?);";
     const char *clear_temp_sql = "DELETE FROM temp_manifest_chunks;";
@@ -12,7 +16,6 @@ extern int fz_query_required_chunk_list(fz_ctx_t *ctx, fz_file_manifest_t *mnfst
         "SELECT f.id, f.chunk_checksum, f.cutpoint, f.chunk_size, f.file_path "
         "FROM filezap_chunks f "
         "JOIN temp_manifest_chunks t ON f.chunk_checksum = t.chunk_checksum;";
-
 
     ret = sqlite3_exec(ctx->db, create_tbl_sql, 0, NULL, NULL);
     if (SQLITE_OK != ret) {
@@ -26,7 +29,6 @@ extern int fz_query_required_chunk_list(fz_ctx_t *ctx, fz_file_manifest_t *mnfst
         RETURN_DEFER(0);
     }
 
-    sqlite3_stmt *insert;
     sqlite3_prepare_v2(ctx->db, insert_sql, -1, &insert, NULL);
     for (size_t i = 0; i < mnfst->chunk_seq.chunk_seq_len; i++){
         sqlite3_bind_int64(insert, 1, mnfst->chunk_seq.chunk_checksum[i]);
@@ -35,7 +37,6 @@ extern int fz_query_required_chunk_list(fz_ctx_t *ctx, fz_file_manifest_t *mnfst
     }
     sqlite3_finalize(insert);
 
-    sqlite3_stmt *stmt;
     ret = sqlite3_prepare_v2(ctx->db, sql, -1, &stmt, NULL);
     if (SQLITE_OK != ret) RETURN_DEFER(0);
 
@@ -65,8 +66,8 @@ extern int fz_query_required_chunk_list(fz_ctx_t *ctx, fz_file_manifest_t *mnfst
     *chunk_buffer = buffer;
     fz_log(FZ_INFO, "chunk size: %lu", local_nchunk);
     defer:
-        if (insert) sqlite3_finalize(insert);
-        if (stmt) sqlite3_finalize(stmt);
+        if (NULL != insert) sqlite3_finalize(insert);
+        if (NULL != stmt) sqlite3_finalize(stmt);
         if (!result && NULL != buffer) {
             for(size_t i = 0; i < local_nchunk; i++){
                 if (NULL != buffer[i].src_file_path) free((char *)buffer[i].src_file_path);
